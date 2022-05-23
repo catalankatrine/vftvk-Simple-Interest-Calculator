f13bc9427b71e90abd17ac9bf473e8502ce10b80
catalankatrine <52690181+catalankatrine@users.noreply.github.com>
2022-05-23 11:11:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+let computeBtn = document.getElementById
+("compute-btn");
+let result = document.getElementById("result");
+let compute = () => {
+    let p = Number(document.getElementById("amount").value);
+    let r = Number(document.getElementById("rate").value);
+    let t = Number(document.getElementById("time").value);
+    let duration = document.getElementById("duration").value;
+
+    let simpleInterest = 
+        duration == "year" ? (p * r * t) / 100 : (p * r * t) / 1200;
+   
+    let principal = p + simpleInterest;
     
-}
-        
+    result.innerHTML = `<div>Principal Amount: <span>$
+        ${p.toFixed(
+            2
+        )}</span></div>
+        <div>Total Interest: <span>$${simpleInterest.toFixed(2)}</span></div>
+        <div>Total Amount: <span>$${principal.toFixed(2)}</span> </div>`;
+};
+computeBtn.addEventListener("click", compute);
+window.addEventListener("load", compute);
